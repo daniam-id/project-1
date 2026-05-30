3f6af0ea617cfa2d1332413da48b66e003fa629c
daniam-id <adam.daniamm@gmail.com>
2026-05-30 20:24:10 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const btnLucky = document.getElementById("btnLucky");
+
+btnLucky.addEventListener("click", () => {
+  alert("Whoops! Fitur 'I'm Feeling Lucky' lagi libur bro! 🚀");
+});
